fee41a7fc194ccc70513a1a532ccc11e9acfaf1e
Kevin Herunter <52201989@edu.campus02.at>
2023-01-31 13:41:16 +0100

    Implementierung Gewinnerkennung

--- connectfour.py
+++ connectfour.py
@@ -63,6 +63,38 @@ class Human(Player):
         pass
 
 
+def check_win(gameboard: list[list[str]]) -> bool:
+    win = False
+    # jede Spalte überprüfen:
+    for i in range(Field.rows - 1, -1, -1):
+        for j in range(0, Field.columns - 3):
+            if (gameboard[i][j] != "-") and (gameboard[i][j] == gameboard[i][j + 1] == gameboard[i][j + 2] == gameboard[i][j + 3]):
+                win = True
+                print("Sieg")
+    # jede Zeile überprüfen:
+    for j in range(Field.columns):
+        for i in range(Field.rows - 4, -1, -1):
+            if (gameboard[i][j] != "-") and (gameboard[i][j] == gameboard[i + 1][j] == gameboard[i + 2][j] == gameboard[i + 3][j]):
+                win = True
+                print("Sieg")
+    # Diagonale überprüfen:
+    # von links unten nach rechts oben:
+    x = Field.rows
+    for i in range(Field.rows - 3):
+        x -= 1
+        for j in range(Field.columns - 3):
+            if (gameboard[x][j] != "-") and (gameboard[x][j] == gameboard[x - 1][j + 1] == gameboard[x - 2][j + 2] == gameboard[x - 3][j + 3]):
+                win = True
+                print("Sieg")
+    # von links oben nach rechts unten: (haut hin)
+    for i in range(Field.rows - 3):
+        for j in range(Field.columns - 3):
+            if (gameboard[i][j] != "-") and (gameboard[i][j] == gameboard[i + 1][j + 1] == gameboard[i + 2][j + 2] == gameboard[i + 3][j + 3]):
+                win = True
+                print("Sieg")
+    return win
+
+
 if __name__ == '__main__':
     # f1 = Field
     # gameboard = f1.create_board(f1)
@@ -92,11 +124,13 @@ if __name__ == '__main__':
                 print("------------Spielfeld--------------")
                 pvpgame.show_field(pvpgame, pvpgameboard)
                 print()
+                check_win(pvpgameboard)
                 player2.throw_token(pvpgameboard)
                 print()
                 print("------------Spielfeld--------------")
                 pvpgame.show_field(pvpgame, pvpgameboard)
                 print()
+                check_win(pvpgameboard)
 
         elif gamemode == "PVC":
             pass
